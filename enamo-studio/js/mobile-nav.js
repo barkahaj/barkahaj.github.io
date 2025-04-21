88acf47bd6105926237f0faed9f8e48ac4b266b9
Barkah Aji <barkah993@gmail.com>
2025-04-21 14:46:56 +0700

    update enamo page -upload

---
+++ enamo-studio/js/mobile-nav.js
@@ -0,0 +1,19 @@
+let mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+let headerMobileNav = document.getElementById('header-mobile-nav');
+let menuIcon = document.getElementById('mobile-menu-icon')
+
+let isChangeColor = false;
+
+let changeBackground = () => {
+    if(isChangeColor) {
+        headerMobileNav.style.display = 'none';
+        menuIcon.src = './img/humberger-menu-icon.png';
+    } else {
+        headerMobileNav.style.display = 'inline-block';
+        menuIcon.src = './img/close-menu-icon.png';
+    }
+
+    isChangeColor = !isChangeColor;
+}
+
+mobileMenuToggle.addEventListener('click', changeBackground)
